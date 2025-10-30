5bdc7a4861794e8f4ab46259f2fadc8c896124e4
Zane Lu <zanelv@xiaoe-tech.com>
2025-10-30 15:51:32 +0800

    feat(scripts): 更改symlink函数名; 取消neovide链接用来暂时避免报错

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,6 @@
 DOTFILES="$HOME/.dotfiles"
 
-symlink() {
+_ln() {
   src=$1
   dest=$2
   backup="$dest.bak"
@@ -25,26 +25,27 @@ symlink() {
 }
 
 # 使用函数创建符号链接
-symlink "$DOTFILES/neovide"                  "$HOME/.config/neovide/"
-symlink "$DOTFILES/ssh/config"               "$HOME/.ssh/config"
-symlink "$DOTFILES/zsh/zshrc"                "$HOME/.zshrc"
-symlink "$DOTFILES/btop"                     "$HOME/.config/btop"
-symlink "$DOTFILES/fd"                       "$HOME/.config/fd"
-symlink "$DOTFILES/fish"                     "$HOME/.config/fish"
-symlink "$DOTFILES/kitty"                    "$HOME/.config/kitty"
-# symlink "$DOTFILES/nvim"                     "$HOME/.config/nvim"
-symlink "$DOTFILES/astronvim"                "$HOME/.config/nvim"
-symlink "$DOTFILES/starship/starship.toml"   "$HOME/.config/starship.toml"
-symlink "$DOTFILES/yazi"                     "$HOME/.config/yazi"
-symlink "$DOTFILES/zellij"                   "$HOME/.config/zellij"
-symlink "$DOTFILES/skhd"                     "$HOME/.config/skhd"
-symlink "$DOTFILES/yabai"                    "$HOME/.config/yabai"
-symlink "$DOTFILES/aerospace/aerospace.toml" "$HOME/.aerospace.toml"
-symlink "$DOTFILES/ghostty"                  "$HOME/.config/ghostty"
-symlink "$DOTFILES/lazygit"                  "$HOME/.config/lazygit"
-symlink "$DOTFILES/gitconfig/.gitconfig"     "$HOME/.gitconfig"
-symlink "$DOTFILES/tmux/tmux.conf"           "$HOME/.tmux.conf"
-symlink "$DOTFILES/tmux"                     "$HOME/.config/tmux"
+# _ln "$DOTFILES/neovide"                  "$HOME/.config/neovide/"
+_ln "$DOTFILES/ssh/config"               "$HOME/.ssh/config"
+_ln "$DOTFILES/zsh/zshrc"                "$HOME/.zshrc"
+_ln "$DOTFILES/btop"                     "$HOME/.config/btop"
+_ln "$DOTFILES/fd"                       "$HOME/.config/fd"
+_ln "$DOTFILES/fish"                     "$HOME/.config/fish"
+_ln "$DOTFILES/kitty"                    "$HOME/.config/kitty"
+# _ln "$DOTFILES/nvim"                     "$HOME/.config/nvim"
+_ln "$DOTFILES/astronvim"                "$HOME/.config/nvim"
+_ln "$DOTFILES/lazyvim"                  "$HOME/.config/lazyvim"
+_ln "$DOTFILES/starship/starship.toml"   "$HOME/.config/starship.toml"
+_ln "$DOTFILES/yazi"                     "$HOME/.config/yazi"
+_ln "$DOTFILES/zellij"                   "$HOME/.config/zellij"
+_ln "$DOTFILES/skhd"                     "$HOME/.config/skhd"
+_ln "$DOTFILES/yabai"                    "$HOME/.config/yabai"
+_ln "$DOTFILES/aerospace/aerospace.toml" "$HOME/.aerospace.toml"
+_ln "$DOTFILES/ghostty"                  "$HOME/.config/ghostty"
+_ln "$DOTFILES/lazygit"                  "$HOME/.config/lazygit"
+_ln "$DOTFILES/gitconfig/gitconfig"      "$HOME/.gitconfig"
+_ln "$DOTFILES/tmux/tmux.conf"           "$HOME/.tmux.conf"
+_ln "$DOTFILES/tmux"                     "$HOME/.config/tmux"
 
 
 # sh $DOTFILES/iterm2/setup.sh
