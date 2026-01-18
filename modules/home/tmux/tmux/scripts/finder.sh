6124e3dc0d67149245269eb78bfdb2e548ea88e4
Zane Lu <lv2497712968@outlook.com>
2026-01-19 03:11:32 +0800

    tmux session finder支持kill session功能

--- modules/home/tmux/tmux/scripts/finder.sh
+++ modules/home/tmux/tmux/scripts/finder.sh
@@ -1,4 +1,17 @@
-tmux list-windows -a -F $'#{?session_attached,1,0}#{?window_active,1,0}#{session_last_attached}\t#{session_name}:#{window_index}\t\e[38;5;245m#{session_name}\e[0m\t\e[38;2;222;222;222m#{window_name}\e[0m' |
-    sort -rnk1 $1 |
-    fzf --tmux --ansi --delimiter=$'\t' --with-nth=3,4  --tabstop=12 --preview 'tmux capture-pane -ep -S -200 -E 1 -t {2}' --preview-window +190 |
-    cut -f 2 | xargs -I {} tmux switch-client -t {}
+SOURCE_CMD="$(cat <<'EOF'
+tmux list-windows -a -F $'#{?session_attached,1,0}#{?window_active,1,0}#{session_last_attached}\t#{session_name}\t#{session_name}:#{window_index}\t\e[38;5;245m#{session_name}\e[0m\t\e[38;2;222;222;222m#{window_name}\e[0m' |
+sort -rnk1
+EOF
+)"
+
+# echo $SOURCE_CMD
+sh -c "$SOURCE_CMD"  |
+fzf --tmux --ansi \
+    --delimiter=$'\t' \
+    --with-nth=4,5 \
+    --tabstop=12 \
+    --preview 'tmux capture-pane -ep -S -200 -E 1 -t {3}' \
+    --preview-window +190 \
+    --bind 'ctrl-x:execute-silent(printf "%s\n" {+2} | xargs -r -n1 tmux kill-session -t)+reload:'"$SOURCE_CMD" |
+cut -f 3 |
+xargs -I {} tmux switch-client -t {}
